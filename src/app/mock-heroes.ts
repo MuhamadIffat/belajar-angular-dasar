81e0cff68cf1be879f8223ae81d2576aa995eb6f
Muhamad Iffat <iffatlathoif@gmail.com>
2020-11-03 06:25:23 +0700

    membuat mock hero

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id:1, name:'Superman'},
+    {id:2, name:'Batman'},
+    {id:3, name:'Iron Man'},
+    {id:4, name:'Ant Man'},
+    {id:5, name:'Thor'},
+    {id:6, name:'Black Phanter'},
+    {id:7, name:'Hulk'},
+    {id:8, name:'Dr Stranger'},
+    {id:9, name:'Wonder Woman'},
+    {id:10, name:'Captain America'},
+]
