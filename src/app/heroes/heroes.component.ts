748008cdb7641edff59ac621bbdd0924efa7cbbd
Muhamad Iffat <iffatlathoif@gmail.com>
2020-11-03 08:01:56 +0700

    fixx code

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,18 +10,19 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 //Refactor this property with interface hero
   // hero = "Windstrom";
-
- selectedHero: Hero;
-
- onSelect(hero: Hero){
-   this.selectedHero = hero;
- }
-
   heroes = HEROES;
+ selectedHero: Hero;
+ 
+
+ 
+
+  
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
 }
